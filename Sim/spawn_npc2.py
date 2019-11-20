3ab907caf1e5a5114bf3d0287cf23eb7b593a415
aroongta <aroongta@andrew.cmu.edu>
2019-11-19 22:17:47 -0500

    Added spawn_npc2.py to spawn vehicles using a random distribution

--- Sim/spawn_npc2.py
+++ Sim/spawn_npc2.py
@@ -1,8 +1,8 @@
 """
-Script for spawning random vehicles around the ROI (Region of Interest).
+Script for spawning random vehicles.
+The spawn points are sampled using a distribution from the list of available spawn points.
 To simulate naturalistic traffic.
-[Spawning of vehicles at the list of availble spawn points, based on a distribution]
-[Spawning vehicles sequentially, checking for collision at each vehicle.]
+[Spawning all vehicles sequentially, checking for collision at each spawn instant.]
 [Ref: spawn_npc.py in Carla/PythonAPI/examples]
 
 Author: Ashish Roongta
@@ -34,24 +34,30 @@ import time
 
 class spawner:
 
-    def __init__(self, map1):
+    def __init__(self, world):
+        # ----------NGSim1-------------------------------------------------
         # self.ROI_in=[[-1720,-1956.5],[-1657, -1928],[-1660, -1838],[-1738,-1887]]
         # self.ROI_ou=[[-1734,-1986.5],[-1631, -1927],[-1631,-1754.5],[-1785,-1870]]
-        self.ROI_in=[[281.13,224.60],[281.13, 269.35],[232.55, 269.35],[232.55, 224.60]]
-        self.ROI_ou=[[311.13,204.60],[311.13, 289.35],[202.55, 289.35],[202.55, 204.60]]
-        self.map=map1
-        self.spawn_points=[]
-        self.spae
+
+        # --------Carla Map Town 04----------------------------
+        self.ROI_in = [[281.13,224.60],[281.13, 269.35],[232.55, 269.35],[232.55, 224.60]]
+        self.ROI_ou = [[311.13,204.60],[311.13, 289.35],[202.55, 289.35],[202.55, 204.60]]
+        self.world = world
+        self.map = world.get_map()
+        self.spawn_points = []
+        self.last_frame = 0
+        self.blueprints = world.get_blueprint_library().filter('vehicle.toyota.*')
+        self.actors=[]
 
     def sign(self,a,b,c):
         return (a[0]-c[0])*(b[1]-c[1])-(b[0]-c[0])*(a[1]-c[1])
 
     def ptinT(self,x,a,b,c):
-        d1=self.sign(x,a,b)
-        d2=self.sign(x,b,c)
-        d3=self.sign(x,c,a)
-        has_neg=(d1<0) or (d2<0) or (d3<0)
-        has_pos=(d1>0) or (d2>0) or (d3>0)
+        d1 = self.sign(x,a,b)
+        d2 = self.sign(x,b,c)
+        d3 = self.sign(x,c,a)
+        has_neg = (d1<0) or (d2<0) or (d3<0)
+        has_pos = (d1>0) or (d2>0) or (d3>0)
         return not(has_neg and has_pos)
     
     def inROI(self,x,ROI):
@@ -78,38 +84,70 @@ class spawner:
         """
         # NGSim1 - lankerShim Boulevard.....................................
         # # delta=20    # Width of surrounding region of the ROI to query spawn points
-        spawn_points=self.map.get_spawn_points()    #   Querying spawnpoints
-        waypoints=self.map.generate_waypoints(5.0)   # Generating waypoints at a resolution of 5m
+        # spawn_points=self.map.get_spawn_points()    #   Querying spawnpoints
+        # waypoints=self.map.generate_waypoints(5.0)   # Generating waypoints at a resolution of 5m
     
-        self.spawn_points=[]
-        waypoints=self.map.generate_waypoints(7.0)
-        for i,w in enumerate(waypoints):
-            x=[w.transform.location.x,-w.transform.location.y]
-            # if i==0:
-            #     wpts=np.array(x).reshape(1,-1)
-            # else:
-            #     wpts=np.vstack((wpts,np.array(x).reshape(1,-1)))
+        # self.spawn_points=[]
+        # for i,w in enumerate(waypoints):
+        #     x=[w.transform.location.x,-w.transform.location.y]
+        #     # if i==0:
+        #     #     wpts=np.array(x).reshape(1,-1)
+        #     # else:
+        #     #     wpts=np.vstack((wpts,np.array(x).reshape(1,-1)))
 
-            if(self.inROI(x,self.ROI_ou) and not(self.inROI(x,self.ROI_in))):
-                spawn_point=w.transform
-                spawn_point.location.z=2
-                self.spawn_points.append(spawn_point)
+        #     if(self.inROI(x,self.ROI_ou) and not(self.inROI(x,self.ROI_in))):
+        #         spawn_point=w.transform
+        #         spawn_point.location.z=2
+        #         self.spawn_points.append(spawn_point)
 
 
         # -------------------------Map Town04, Unsignalied 4way intersection
         
-        # self.spawn_points=self.map.get_spawn_points()[:4]
-        # spawnx=[255.257156, 258.637909, 294.50, 222.266479 ]
-        # spawny=[-282.577789, -209.931717, -250.234390, -245.930328]
-        # spawnyaw=[90.172272, -88.316437, -179.65, -0.500977]
-        # for i in range(4):
-        #     self.spawn_points[i].location.x=spawnx[i]
-        #     self.spawn_points[i].location.y=spawny[i]
-        #     self.spawn_points[i].rotation.yaw=spawnyaw[i]
-        #     # self.spawn_points.append(spawn_point)
+        self.spawn_points=self.map.get_spawn_points()[:4]
+        spawnx=[255.257156, 258.637909, 294.50, 222.266479 ]
+        spawny=[-282.577789, -209.931717, -250.234390, -245.930328]
+        spawnyaw=[90.172272, -88.316437, -179.65, -0.500977]
+        for i in range(4):
+            self.spawn_points[i].location.x=spawnx[i]
+            self.spawn_points[i].location.y=spawny[i]
+            self.spawn_points[i].rotation.yaw=spawnyaw[i]
+            # self.spawn_points.append(spawn_point)
         
         print('Number of spawn points in the hanger: ',len(self.spawn_points))
 
+    def spawn_vehicles(self, frame, df=10):
+        """
+        Function to spawn vehicles at the list of spawn points.
+        """
+        if not(frame%10==0):
+            return 
+        else:
+            # spts=random.choice(len(self.spawnpoints))
+            for i in range(len(self.spawnpoints)):
+                # Randomly choosing spawn points, (Naturalisitic Traffic simulation)
+                spawn_point = random.choice(self.spawnpoints)     
+                blueprint = random.choice(self.blueprints)
+                actor = self.world.try_spawn_actor(blueprint, spawn_point)
+                actor.set_autopilot()   # Setting the vehicle control in autpolit mode
+                self.actors.append(actor)
+
+    def check_VState(self):
+        """
+        To remove the vehicles (kill the actors) that have moved out of the region of interest...
+        """
+        for actors in self.actors:
+            lc = actor.get_transform().location
+            if not(self.inROI([lc.x, -lc.y], ROI.ou)):
+                actor.destroy()
+                self.actors.remove(actor)
+
+
+    def destroy_actors(self):
+        """
+        To destroy all the actors in the simulation
+        """
+        for actor in self.actors:
+            actor.destroy()
 
 
 def main():
@@ -126,94 +164,41 @@ def main():
         default=2000,
         type=int,
         help='TCP port to listen to (default: 2000)')
-    argparser.add_argument(
-        '-n', '--number-of-vehicles',
-        metavar='N',
-        default=10,
-        type=int,
-        help='number of vehicles (default: 10)')
-    argparser.add_argument(
-        '-w', '--number-of-walkers',
-        metavar='W',
-        default=50,
-        type=int,
-        help='number of walkers (default: 50)')
-    argparser.add_argument(
-        '--safe',
-        action='store_true',
-        help='avoid spawning vehicles prone to accidents')
-    argparser.add_argument(
-        '--filterv',
-        metavar='PATTERN',
-        default='vehicle.*',
-        help='vehicles filter (default: "vehicle.*")')
-    argparser.add_argument(
-        '--filterw',
-        metavar='PATTERN',
-        default='walker.pedestrian.*',
-        help='pedestrians filter (default: "walker.pedestrian.*")')
+    argparse.add_argument(
+        '--fps',
+        default=30
+        type=int
+        help='The simulation fps')
     args = argparser.parse_args()
 
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    vehicles_list = []
-    walkers_list = []
-    all_id = []
-    client = carla.Client(args.host, args.port)
-    client.set_timeout(2.0)
-    spawnNow=True
     try:
-        
+        client = carla.Client(args.host, args.port)
+        client.set_timeout(3.0)
         world = client.get_world()
-        garage=spawner(world.get_map())
-        blueprints = world.get_blueprint_library().filter('vehicle.toyota.*')
 
-        # spawn_points = world.get_map().get_spawn_points()
-        garage.spawn_hangar()
-        spawn_points=garage.spawn_points
+        # Instantiating an object of the spawner class
+        garage=spawner(world)   
 
-        number_of_spawn_points = len(spawn_points)
-        # print('num of waypoints',number_of_spawn_points)
+        # Configuring the simulation to run at a constant fps and in sycnhronous mode.
+        fps = args.fps
+        settings = world.get_settings()
+        settings.sycnhronous_mode = True
+        settings.fixed_delta_seconds = 1.0/fps
+        world.apply_settings(settings)
 
-        # @todo cannot import these directly.
-        SpawnActor = carla.command.SpawnActor
-        SetAutopilot = carla.command.SetAutopilot
-        FutureActor = carla.command.FutureActor
         while(True):
-            world.wait_for_tick()
-            # --------------
-            # Spawn vehicles
-            # --------------
-            batch=[]
-            if spawnNow:
-                for n,waypt in enumerate(spawn_points):
-                    transform=waypt
-                    blueprint=random.choice(blueprints)
-                    batch.append(SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True)))
-                client.apply_batch_sync(batch)
-                spawnNow=False
+            frame = world.tick()
+            print(frame)
+            # Spawning Vehicles based on random distribution
+            garage.spawn_vehicles(frame)
 
-            vehicles_list=world.get_actors().filter('vehicle.*')
-            if(len(vehicles_list)<=0):
-                spawnNow=True
-
-            for car in vehicles_list:
-                lc=car.get_transform().location
-                if not(garage.inROI([lc.x,-lc.y],garage.ROI_ou)):
-                    car.destroy()
-
+            # Checking and removing vehicles that have moved outside ROI
+            garage.check_VState()
     finally:
-
-        print('\ndestroying %d vehicles' % len(vehicles_list))
-        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
-
-        # stop walker controllers (list is [controler, actor, controller, actor ...])
-        for i in range(0, len(all_id), 2):
-            all_actors[i].stop()
-
-        print('\ndestroying %d walkers' % len(walkers_list))
-        client.apply_batch([carla.command.DestroyActor(x) for x in all_id])
-
+        print("Entered Finally, destroying {} vehicles".format(len(garage.actors)))
+        garage.destroy_actors()   
 
 if __name__ == '__main__':
 
